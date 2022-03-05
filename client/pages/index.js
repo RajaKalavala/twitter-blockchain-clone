35b5c3cbb6421309c9a7dc3325ede07842eb0a4a
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-05 23:22:25 +0530

    Change tsx to js and Add Sidebar component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,18 @@
-import type { NextPage } from 'next'
+import Sidebar from '../components/Sidebar'
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
   content: `maz-w-[1400px] w-2/3 flex justify-between`,
 }
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <div className={style.wrapper}>
       <div className={style.content}>
-        <h2>Sidebar</h2>
+        <Sidebar />
         <h2>Feed</h2>
         <h2>Widgets</h2>
       </div>
     </div>
   )
 }
-
-export default Home
