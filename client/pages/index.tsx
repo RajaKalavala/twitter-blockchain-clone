a4e7a39207648ae744a42bcb36a8261a3587f9a7
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-05 23:04:09 +0530

    Add Style wrapper
    Add style to split the different components

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+
+const style = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `maz-w-[1400px] w-2/3 flex justify-between`,
+}
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h2>Sidebar</h2>
-      <h2>Feed</h2>
-      <h2>Widgets</h2>
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <h2>Sidebar</h2>
+        <h2>Feed</h2>
+        <h2>Widgets</h2>
+      </div>
     </div>
   )
 }
