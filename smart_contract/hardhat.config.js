fa077612b13eb581f772c272af6cc938e1c821ed
Raja Kalavala <rajakalavala55@gmail.com>
2022-03-07 00:17:02 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,9 +4,11 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/QpnCoroFLlnjxbaRoGOhsZMowHBvzg68',
+      url:
+        "https://eth-rinkeby.alchemyapi.io/v2/QpnCoroFLlnjxbaRoGOhsZMowHBvzg68",
       accounts: [
         "c8a07b42550504251beb64faa0c282e68ff6d5869b749fb5fbc996dd39bbe3bb",
-      ]
+      ],
+    },
   },
 };
