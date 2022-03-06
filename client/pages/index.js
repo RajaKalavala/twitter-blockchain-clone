70003cd38d26d5e343855618cabc1d70b52d3aff
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-06 20:10:54 +0530

    Add widget.js and static data

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/home/Feed'
+import Widgets from '../components/Widgets'
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
@@ -12,7 +13,7 @@ export default function Home() {
       <div className={style.content}>
         <Sidebar />
         <Feed />
-        <h2>Widgets</h2>
+        <Widgets />
       </div>
     </div>
   )
