16274a0dd6fb998d545ebe5bca96f48ae540d2a9
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-07 00:08:25 +0530

    Add Alchemy api url

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "",
+      url: "https://eth-rinkeby.alchemyapi.io/v2/QpnCoroFLlnjxbaRoGOhsZMowHBvzg68",
       accounts: [""],
     },
   },
