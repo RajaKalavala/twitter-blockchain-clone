6add06812861ca770d33de755935a7e7c22b9b12
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-06 21:07:35 +0530

    Finish ProfileTweet.
    Finish FE part now start SmartContract

--- client/components/profile/ProfileTweets.js
+++ client/components/profile/ProfileTweets.js
@@ -1,7 +1,69 @@
 import React from 'react'
+import Post from '../Post'
+
+const style = {
+  wrapper: `no-scrollbar`,
+  header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
+  headerTitle: `text-xl font-bold`,
+}
+
+const tweets = [
+  {
+    displayName: 'Raja',
+    userName: 'abcdefghijklmanopqrstuvwxyz',
+    avatar:
+      'https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2020-06-01T12:00:00:000Z',
+  },
+  {
+    displayName: 'Raja',
+    userName: 'abcdefghijklmanopqrstuvwxyz',
+    avatar:
+      'https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2020-06-01T12:00:00:000Z',
+  },
+  {
+    displayName: 'Raja',
+    userName: 'abcdefghijklmanopqrstuvwxyz',
+    avatar:
+      'https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2020-06-01T12:00:00:000Z',
+  },
+  {
+    displayName: 'Raja',
+    userName: 'abcdefghijklmanopqrstuvwxyz',
+    avatar:
+      'https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2020-06-01T12:00:00:000Z',
+  },
+]
 
 const ProfileTweets = () => {
-  return <div>ProfileTweets</div>
+  return (
+    <div className={style.wrapper}>
+      {tweets.map((tweet, index) => (
+        <Post
+          key={index}
+          displayName="Raja Kalavala"
+          userName={`${tweet.userName.slice(0, 4)}...${tweet.userName.slice(
+            -4
+          )}`}
+          text={tweet.text}
+          avatar={tweet.avatar}
+          isProfileImageNft={tweet.isProfileImageNft}
+          timestamp={tweet.timestamp}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default ProfileTweets
