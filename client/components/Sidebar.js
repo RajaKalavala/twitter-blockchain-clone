398c9956110cd1c1dc6a0c2d4acb1f10679457d1
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-06 11:30:43 +0530

    Update data with SidebarOption in Sidebar component

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,7 +1,18 @@
 import { useState } from 'react'
-import { FiMoreHorizontal } from 'react-icons/fi'
 import { VscTwitter } from 'react-icons/vsc'
 import SidebarOption from '../components/Sidebaroption'
+import { RiHome7Line, RiHome7Fill, RiFileList2Fill } from 'react-icons/ri'
+import { BiHash } from 'react-icons/bi'
+import { FiBell, FiMoreHorizontal } from 'react-icons/fi'
+import { HiOutlineMail, HiMail } from 'react-icons/hi'
+import { FaRegListAlt, FaHashtag, FaBell } from 'react-icons/fa'
+import { CgMoreO } from 'react-icons/cg'
+import {
+  BsBookmark,
+  BsBookmarkFill,
+  BsPerson,
+  BsPersonFill,
+} from 'react-icons/bs'
 
 const style = {
   wrapper: `flex-[0.7] px-8 flex flex-col`,
@@ -27,20 +38,51 @@ function Sidebar({ initialSelectedIcon = 'Home' }) {
       </div>
       <div className={style.navContainer}>
         <SidebarOption
-          Icon={selected === 'Home' ? VscTwitter : VscTwitter}
+          Icon={selected === 'Home' ? RiHome7Fill : RiHome7Line}
           text="Home"
           isActive={Boolean(selected === 'Home')}
           setSelected={setSelected}
           redirect={'/'}
         />
-        <div>Home</div>
-        <div>Explore</div>
-        <div>Notifications</div>
-        <div>Messages</div>
-        <div>Bookmarks</div>
-        <div>Lists</div>
-        <div>Profile</div>
-        <div>More</div>
+
+        <SidebarOption
+          Icon={selected === 'Explore' ? FaHashtag : BiHash}
+          text="Explore"
+          isActive={Boolean(selected === 'Explore')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Notifications' ? FaBell : FiBell}
+          text="Notifications"
+          isActive={Boolean(selected === 'Notifications')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Messages' ? HiMail : HiOutlineMail}
+          text="Messages"
+          isActive={Boolean(selected === 'Messages')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Bookmarks' ? BsBookmarkFill : BsBookmark}
+          text="Bookmarks"
+          isActive={Boolean(selected === 'Bookmarks')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Lists' ? RiFileList2Fill : FaRegListAlt}
+          text="Lists"
+          isActive={Boolean(selected === 'Lists')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Profile' ? BsPersonFill : BsPerson}
+          text="Profile"
+          isActive={Boolean(selected === 'Profile')}
+          setSelected={setSelected}
+          redirect={'/profile'}
+        />
+        <SidebarOption Icon={CgMoreO} text="More"></SidebarOption>
         <div className={style.tweenButton}>Mint</div>
       </div>
       <div className={style.profileButton}>
