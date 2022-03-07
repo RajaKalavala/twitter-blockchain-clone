1373c46db5f8330ef91dcb00a500ed8c86180020
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-07 22:45:53 +0530

    Initialize metamask wallet connection

--- client/context/TwitterContext.js
+++ client/context/TwitterContext.js
@@ -1,21 +1,66 @@
 import { createContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 export const TwitterContext = createContext()
 
 export const TwitterProvider = ({ children }) => {
+  const [appStatus, setAppStatus] = useState('loading')
+  const [currentAccount, setCurrentAccount] = useState('')
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
+
+  /**
+   * Checks if there is an active wallet connection
+   */
   const checkIfWalletIsConnected = async () => {
-    if (!window.ethereum) return
+    if (!window.ethereum) return setAppStatus('noMetaMask')
     try {
       const addressArray = await window.ethereum.request({
         method: 'eth_accounts',
       })
       if (addressArray.length > 0) {
         //connected
+        setAppStatus('connected')
+        setCurrentAccount(addressArray[0])
       } else {
         // not connected
+        router.push('/')
+        setAppStatus('notConnected')
       }
     } catch (error) {
       console.log(error)
     }
   }
+
+  /**
+   * Initiates MetaMask wallet connection
+   */
+  const connectWallet = async () => {
+    if (!window.ethereum) return setAppStatus('noMetaMask')
+    try {
+      setAppStatus('loading')
+
+      const addressArray = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+
+      if (addressArray.length > 0) {
+        setCurrentAccount(addressArray[0])
+        createUserAccount(addressArray[0])
+      } else {
+        router.push('/')
+        setAppStatus('notConnected')
+      }
+    } catch (err) {
+      setAppStatus('error')
+    }
+  }
+
+  return (
+    <TwitterContext.Provider value={{ appStatus, currentAccount, conn }}>
+      {children}
+    </TwitterContext.Provider>
+  )
 }
