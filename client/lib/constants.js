e7c13fcdca9289851021b015b5ebd88ee5119015
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-07 22:28:23 +0530

    Add ContractAddress and contractABI

---
+++ client/lib/constants.js
@@ -0,0 +1,4 @@
+import ProfileImageMinterContractAbi from './ProfileImageNfts.json'
+
+export const contractAddress = '0xA73FFf0C7f06a90398Bf507687EBec372a172711'
+export const contractABI = ProfileImageMinterContractAbi.abi
