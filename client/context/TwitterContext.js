86eee1c63e48098af37458550513d0aa95408011
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-10 22:13:01 +0530

    Add Sanity.io Backend connection to create user in Sanity

--- client/context/TwitterContext.js
+++ client/context/TwitterContext.js
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { client } from '../lib/client'
 
 export const TwitterContext = createContext()
 
@@ -25,6 +26,7 @@ export const TwitterProvider = ({ children }) => {
         //connected
         setAppStatus('connected')
         setCurrentAccount(addressArray[0])
+        createUserAccount(addressArray[0])
       } else {
         // not connected
         router.push('/')
@@ -50,12 +52,38 @@ export const TwitterProvider = ({ children }) => {
       if (addressArray.length > 0) {
         setCurrentAccount(addressArray[0])
         setAppStatus('connected')
-        //createUserAccount(addressArray[0])
+        createUserAccount(addressArray[0])
       } else {
         router.push('/')
         setAppStatus('notConnected')
       }
     } catch (err) {
+      setAppStatus('error')
+    }
+  }
+
+  /**
+   * Creates an account in Sanity DB if the user does not already have one
+   * @param {String} userAddress Wallet address of the currently logged in user
+   */
+  const createUserAccount = async (userAddress = currentAccount) => {
+    if (!window.ethereum) return setAppStatus('noMetaMask')
+    try {
+      const userDoc = {
+        _type: 'users',
+        _id: userAddress,
+        name: 'Unnamed',
+        isProfileImageNft: false,
+        profileImage:
+          'https://about.twitter.com/content/dam/about-twitter/en/brand-toolkit/brand-download-img-1.jpg.twimg.1920.jpg',
+        walletAddress: userAddress,
+      }
+
+      await client.createIfNotExists(userDoc)
+
+      setAppStatus('connected')
+    } catch (error) {
+      router.push('/')
       setAppStatus('error')
     }
   }
